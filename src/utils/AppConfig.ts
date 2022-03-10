668c98527a152576cf4ebcc4b424dea135c71e45
LeeLeeLeeee <ijj1792@naver.com>
2022-03-10 21:10:06 +0900

    feat: ESLINT, CONFIG 환경 세팅

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
-  locale: 'en',
+  site_name: 'YHLEE Dev Blog',
+  title: 'Dev!',
+  description: '이영현 기술 블로그',
+  locale: 'ko',
 };
