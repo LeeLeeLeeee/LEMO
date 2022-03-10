78018b3ef6d50d98ca2e8dbfefaf1c9a0e57e436
LeeLeeLeeee <ijj1792@naver.com>
2022-03-10 21:45:44 +0900

    feat: NextJS 환경 변경

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,17 +4,19 @@ import { AppConfig } from '@/utils/AppConfig';
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang={AppConfig.locale}>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render() {
+        return (
+            <Html lang={AppConfig.locale}>
+                <Head>
+                    <meta charSet="utf-8"></meta>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
 
 export default MyDocument;
