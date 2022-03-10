989e0f222df49de6c2a24c18bfde56db4801b1a0
LeeLeeLeeee <ijj1792@naver.com>
2022-03-10 21:46:17 +0900

    lint 적용

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'YHLEE Dev Blog',
-  title: 'Dev!',
-  description: '이영현 기술 블로그',
-  locale: 'ko',
+    site_name: 'YHLEE Dev Blog',
+    title: 'Dev!',
+    description: '이영현 기술 블로그',
+    locale: 'ko',
 };
