3a05f6cddcd91c80509cc6af30c6ca92a374f347
LeeLeeLeeee <ijj1792@naver.com>
2022-03-23 20:59:03 +0900

    eslint 설정 반영

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,25 +1,27 @@
-import { AppProps } from 'next/app';
-import GlobalStyles from '@/layout/GlobalStyles';
-import '../styles/global.css';
-import HeaderComponent from '@/components/common/Header';
 import React, { useEffect } from 'react';
-import * as eva from "eva-icons";
-import { Provider } from 'react-redux'
+
+import { AppProps } from 'next/app';
+import '../styles/global.css';
+import { Provider } from 'react-redux';
+import * as eva from 'eva-icons';
+
+import HeaderComponent from '@/components/common/Header';
 import { useStore } from '@/stores/store';
 import { useSettingsState } from '@/stores/setting/hook';
 import { BaseChild } from '@/components/interface';
+import GlobalStyles from '@/layout/GlobalStyles';
 
 const AppWrapper = React.memo((props: BaseChild) => {
     const { mode } = useSettingsState();
     return (
-        <div className={mode} style={{ width: '100%', height: '100%'}}>
+        <div className={mode} style={{ width: '100%', height: '100%' }}>
             {props.children}
         </div>
     );
 });
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-    const store = useStore(pageProps.initialReduxState)
+    const store = useStore(pageProps.initialReduxState);
     useEffect(() => {
         eva.replace({
             type: 'zoom',
@@ -37,7 +39,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
                 </>
             </AppWrapper>
         </Provider>
-    )
+    );
 };
 
 export default MyApp;
