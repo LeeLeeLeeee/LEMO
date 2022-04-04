aebd53eb8a75f87d63f4f47c11ed8d3c4f64d3e5
LeeLeeLeeee <ijj1792@naver.com>
2022-04-05 00:17:45 +0900

    feat: svg 컴포넌트 연동

--- front/src/icons.tsx
+++ front/src/icons.tsx
@@ -6,6 +6,7 @@ import SVGHeartIcon from './svgs/heart-svgrepo-com.svg';
 import SVGPhoneIcon from './svgs/phone-svgrepo-com.svg';
 import SVGMoonIcon from './svgs/moon-svgrepo-com.svg';
 import SVGSunIcon from './svgs/sun-svgrepo-com.svg';
+import SVGSaveIcon from './svgs/save-svgrepo-com.svg';
 
 export const GithubIcon = React.memo((): JSX.Element => <SVGGithubIcon />);
 export const EmailIcon = React.memo((): JSX.Element => <SVGEmailIcon />);
@@ -13,3 +14,4 @@ export const HeartIcon = React.memo((): JSX.Element => <SVGHeartIcon />);
 export const PhoneIcon = React.memo((): JSX.Element => <SVGPhoneIcon />);
 export const MoonIcon = React.memo((): JSX.Element => <SVGMoonIcon />);
 export const SunIcon = React.memo((): JSX.Element => <SVGSunIcon />);
+export const SaveIcon = React.memo((): JSX.Element => <SVGSaveIcon />);
