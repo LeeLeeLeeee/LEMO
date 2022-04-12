98ffd71302f15a215f3daee2fee26d4edd26643b
LeeLeeLeeee <ijj1792@naver.com>
2022-04-13 00:12:08 +0900

    feat: eslint 충돌 제거

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
           "tabWidth": 4
         }
     ],
-    '@typescript-eslint/indent': ['error', 4],
+    '@typescript-eslint/indent': ['off'],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
