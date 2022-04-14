b270f6154025db21e5e8d7f835176519844b0d99
LeeLeeLeeee <ijj1792@naver.com>
2022-04-14 22:52:29 +0900

    feat: .husky, lint-stage 설정

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,33 @@
-module.exports = {
-  'front/*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
-  'front/**/*.ts?(x)': () => 'npm run build-types',
-  'front/*.json': ['prettier --write'],
+const micromatch = require('micromatch');
+
+function containsInPath(pattern, list) {
+    return list.filter((item) => micromatch.contains(item, pattern));
+}
+
+function makePattern(extension) {
+    return `**/*.${extension}`;
+}
+
+module.exports = (stagedFiles) => {
+    const eslintExtensions = ['ts', 'tsx', 'js'].map(makePattern);
+    const eslintFiles = micromatch(stagedFiles, eslintExtensions);
+
+    const front = containsInPath('/front/', eslintFiles);
+    const core = containsInPath('/core/', eslintFiles);
+    const server = containsInPath('/server/', eslintFiles);
+
+    const mapping = {};
+    const commands = [];
+    mapping['npm run precommit:front -- '] = front.join(' ');
+    mapping['npm run precommit:core -- '] = core.join(' ');
+    mapping['npm run precommit:server -- '] = server.join(' ');
+
+    for (const command of Object.keys(mapping)) {
+        const files = mapping[command];
+        if (files.length) {
+            commands.push(`${command} ${files}`);
+        }
+    }
+
+    return commands;
 };
